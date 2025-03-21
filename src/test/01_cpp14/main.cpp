71d5100ff698ba15077902e353e155d46124bd17
John <yangtianzhuo970425@gmail.com>
2025-03-22 00:40:16 +1300

    Bump project version to 0.1.3, update ANTLR download link, and add CMake configuration for ParserCpp14

--- src/test/01_cpp14/main.cpp
+++ src/test/01_cpp14/main.cpp
@@ -1,5 +1,5 @@
-/* Copyright (c) 2012-2017 The ANTLR Project. All rights reserved.
- * Use of this file is governed by the BSD 3-clause license that
+﻿/* Copyright (c) 2012-2017 The ANTLR Project. All rights reserved.
+* Use of this file is governed by the BSD 3-clause license that
  * can be found in the LICENSE.txt file in the project root.
  */
 
@@ -10,13 +10,14 @@
 //  Created by Mike Lischke on 13.03.16.
 //
 
-#include <UANTLR/ParserCpp14/CPP14BaseVisitor.h>
-#include <UANTLR/ParserCpp14/CPP14Lexer.h>
-#include <UANTLR/ParserCpp14/CPP14Parser.h>
-#include <Windows.h>
+#include <iostream>
+
+#include <MyANTLR/ParserCpp14/CPP14BaseVisitor.h>
+#include <MyANTLR/ParserCpp14/CPP14Lexer.h>
+#include <MyANTLR/ParserCpp14/CPP14Parser.h>
 #include <antlr4-runtime.h>
 
-#include <iostream>
+#include <Windows.h>
 
 #pragma execution_character_set("utf-8")
 
@@ -25,11 +26,11 @@ using namespace antlr4;
 
 int main(int argc, const char* argv[]) {
   ANTLRInputStream input(R"(
- namespace A::B {
-   struct [[meta("hello world")]] Cmpt{
-   };
- }
- )");
+namespace A::B {
+  struct [[meta("hello world")]] Cmpt{
+  };
+}
+)");
   CPP14Lexer lexer(&input);
   CommonTokenStream tokens(&lexer);
 
